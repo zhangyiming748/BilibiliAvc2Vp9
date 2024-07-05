d1c98e2e600068f377521ae913f5999119784a76
zen <zhangyiming748@gmail.com>
2024-07-05 16:05:10 +0800

    加入进度条功能

--- vp9/vp9.go
+++ vp9/vp9.go
@@ -1,6 +1,7 @@
 package vp9
 
 import (
+	"BilibiliAvc2Vp9/util"
 	"github.com/zhangyiming748/FastMediaInfo"
 	"log"
 	"os"
@@ -20,12 +21,13 @@ func ToVP9(fp string) {
 	mi := FastMediaInfo.GetStandMediaInfo(fp)
 	width, _ := strconv.Atoi(mi.Image.Width)
 	height, _ := strconv.Atoi(mi.Image.Height)
-	framecount := mi.Video.FrameCount
-	log.Printf("文件%v的分辨率是%vx%v,帧数是%v\n", fp, width, height, framecount)
+	frameCount := mi.Video.FrameCount
+	log.Printf("文件%v的分辨率是%vx%v,帧数是%v\n", fp, width, height, frameCount)
 	crf := FastMediaInfo.GetCRF("vp9", width, height)
 	if crf == "" {
 		crf = "31"
 	}
+
 	if mi.Video.Format == "AVC" {
 		log.Printf("文件%v是h264格式,转换\n", fp)
 	} else {
@@ -34,19 +36,19 @@ func ToVP9(fp string) {
 	}
 	cmd := exec.Command("ffmpeg", "-y", "-i", fp, "-map", "0:v:0", "-map", "0:a:0", "-map", "0:s:0", "-c:v", "libvpx-vp9", "-crf", crf, "-c:a", "libopus", "-vbr", "on", "-c:s", "copy", out)
 	log.Printf("开始执行命令%v\n", cmd.String())
-	output, err := cmd.CombinedOutput()
+	err := util.ExecCommand(cmd, frameCount)
 	if err != nil {
 		log.Printf("命令%v执行失败%v有可能不包括字幕\n", cmd.String(), err)
 		cmd = exec.Command("ffmpeg", "-y", "-i", fp, "-map", "0:v:0", "-map", "0:a:0", "-c:v", "libvpx-vp9", "-crf", crf, "-c:a", "libopus", "-vbr", "on", out)
 		log.Printf("开始执行命令%v\n", cmd.String())
-		if output, err = cmd.CombinedOutput(); err != nil {
+		if err = util.ExecCommand(cmd, frameCount); err != nil {
 			log.Fatalf("命令不带字幕%v执行失败%v\n", cmd.String(), err)
 		} else {
-			log.Printf("命令成功执行%v\n", string(output))
+			log.Printf("命令成功执行%v\n", cmd.String())
 			os.Remove(fp)
 		}
 	} else {
-		log.Printf("命令成功执行%v\n", string(output))
+		log.Printf("命令成功执行%v\n", cmd.String())
 		os.Remove(fp)
 	}
 }
